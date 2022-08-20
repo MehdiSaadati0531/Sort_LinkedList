574ac8165259f74e781e0c5903c4a81c73bb80d5
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-20 11:08:04 +0430

    Add files via upload

---
+++ Sort_LinkedList/Implement.cs
@@ -0,0 +1,86 @@
+﻿namespace Sort_LinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class NodeOperation
+    {
+        Node head = null;
+
+        private bool IsNull()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Insert(int val)
+        {
+            Node newNode = new Node();
+            newNode.Value = val;
+            if (IsNull())
+            {
+                head = newNode;
+                head.Next = null;
+            }
+            else
+            {
+                Node finde = head;
+                while (finde != null)
+                {
+                    if (val >= finde.Value)
+                    {
+                        if (finde.Next != null)
+                        {
+                            if (val <= finde.Next.Value)
+                            {
+                                newNode.Next = finde.Next;
+                                finde.Next = newNode;
+                                break;
+                            }
+                            finde = finde.Next;
+                            continue;
+                        }
+                        else
+                        {
+                            finde.Next = newNode;
+                            newNode.Next = null;
+                            break;
+                        }
+                    }
+                    else if (finde.Value >= val)
+                    {
+                        newNode.Next = finde;
+                        head = newNode;
+                        break;
+                    }
+                    else if (finde.Next == null)
+                    {
+                        finde.Next = newNode;
+                        newNode.Next = null;
+                        break;
+                    }
+                    finde = finde.Next;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Value);
+                temp = temp.Next;
+            }
+        }
+    }
+}
---
+++ Sort_LinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿using Sort_LinkedList;
+
+
+NodeOperation a = new NodeOperation();
+
+a.Insert(1);
+a.Insert(5);
+a.Insert(6);
+a.Insert(0);
+a.Insert(2);
+a.Insert(50);
+a.Insert(35);
+
+a.Print();
